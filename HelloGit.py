eb1c219ffd311b296ac32a146501897566286859
vlkan <devpwdkernel@gmail.com>
2019-09-05 16:59:08 +0300

    new commit

---
+++ HelloGit.py
@@ -0,0 +1,12 @@
+class SayHi:
+
+    def __init__(self,name):
+        self.name=name
+
+name=input("What is your name? ")
+#redpillorbluepill.
+matrix=SayHi(name)
+
+print("Hello {}!".format(matrix.name))
+
+##HelloNeo!.
